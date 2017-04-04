4d02afad3758378fcd9f9ca0210b447bb2e8c496
yuliani <inailuy@me.cm>
2017-04-04 11:54:01 -0500

    enable background capabilities

--- music_player/AppDelegate.swift
+++ music_player/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // Enable Background
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayback, with: .duckOthers)
+        try! session.setActive(true)
+        
         return true
     }
 }
